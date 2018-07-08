eae65096ccb24feb3a335b34510c5f69e0306882
orjan <orjanbostad.vesterlid@gmail.com>
2018-07-08 19:34:40 +0200

    Initial db-connection and routing for getting members.

--- server/src/app.py
+++ server/src/app.py
@@ -1,54 +1,25 @@
 from aiohttp import web
-import aiomysql
 import asyncio
 import os
 
-app = web.Application()
+import server.src.members
 
 
 def get_environ_sfe(name, default=None):
-
-    if not name in os.environ:
-      return default
+    if name not in os.environ:
+        return default
     else:
         return os.environ[name]
 
 
-DB_PASSWORD = get_environ_sfe("SALT_PASSWD")
-DB_USER = "salt_linjeforening"
-DB_NAME = "salt_linjeforening"
-
-
-async def mysql_connect():
-    conn = await aiomysql.connect(host='tihlde.org', port=3306,
-                                       user=DB_USER, password=DB_PASSWORD,
-                                       db=DB_NAME, loop=loop)
-    cur = await conn.cursor()
-    return conn, cur
-
-
-async def get_all_users():
-    (conn, cur) = await mysql_connect()
-
-    await cur.execute("SELECT * FROM user")
-    print(cur.description)
-    r = await cur.fetchall()
-    print(r)
-    await cur.close()
-    conn.close()
-
-
-async def get_user(name):
-    (conn, cur) = await mysql_connect()
-
-    await cur.execute("SELECT * FROM user WHERE  first_name = %s OR last_name = %s", (name, name))
-    r = await cur.fetchall()
-    print(r)
-    await cur.close()
-    conn.close()
-
+app = web.Application()
 
 loop = asyncio.get_event_loop()
-#loop.run_until_complete(get_all_users())
-loop.run_until_complete(get_user("Kevin"))
 
+if __name__ == '__main__':
+    server.src.members.init(loop)
+
+    app.add_routes([web.get('/api/user/{name}', server.src.members.get_member),
+                    web.get('/api/allusers', server.src.members.get_all_members)])
+
+    web.run_app(app)
