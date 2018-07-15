7fc304aa547a8be377840b14fb1a5fa199989c54
Gard Steinsvik <gardsteinsvik@gmail.com>
2018-07-15 16:13:24 +0200

    Added grid and styling to registration form

--- client/src/Components/UserRegistrationForm/UserRegistrationForm.js
+++ client/src/Components/UserRegistrationForm/UserRegistrationForm.js
@@ -1,5 +1,16 @@
 import React, { Component } from "react";
-import { Button, Input, InputLabel, Checkbox, Select, MenuItem, FormControl } from "@material-ui/core";
+import { Button, Input, InputLabel, Grid, Checkbox, Select, MenuItem, FormControl, Typography } from "@material-ui/core";
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+    formControl: {
+        margin: theme.spacing.unit,
+        minWidth: 180,
+    },
+    selectEmpty: {
+        marginTop: theme.spacing.unit * 2,
+    },
+});
 
 class UserRegistrationForm extends Component {
 
@@ -10,82 +21,109 @@ class UserRegistrationForm extends Component {
         privateEmail: '',
         studyProgramme: '',
         yearOfAdmission: '',
+        vippsTransactionCode: '',
         wantNewsletter: false,
-        vippsTransactionCode: ''
+        acceptTermsOfService: false
     };
 
-    handleChange = event => {        
+    handleChange = event => {
         this.setState({ [event.target.name]: event.target.value });
     };
 
     render() {
+        const { classes } = this.props;
+
         return (
             <div>
-                <h2>Registrering</h2>
+                <Typography variant="title">Registrering</Typography>
                 <form>
-                    <FormControl>
-                        <InputLabel>Fornavn:</InputLabel>
-                        <Input />
-                    </FormControl>
-                    <FormControl>
-                        <InputLabel>Etternavn:</InputLabel>
-                        <Input />
-                    </FormControl>
-                    <FormControl>
-                        <InputLabel>Student-epost:</InputLabel>
-                        <Input />
-                    </FormControl>
-                    <FormControl>
-                        <InputLabel>Privat epost:</InputLabel>
-                        <Input />
-                    </FormControl>
-                    <FormControl>
-                        <InputLabel htmlFor="study-programme">Studieprogram</InputLabel>
-                        <Select
-                            value={this.state.studyProgramme}
-                            onChange={this.handleChange}
-                            inputProps={{
-                                name: 'studyProgramme',
-                                id: 'study-programme',
-                            }}
-                        >
-                            <MenuItem value={0}>MGLU1-7 - Grunnskolelærerutdanning 1.–7. trinn</MenuItem>
-                        </Select>
-                    </FormControl>
-                    <FormControl>
-                        <InputLabel htmlFor="year-of-admission">
-                            Opptaksår
+                    <Grid container spacing={8}>
+                        <Grid item xs={6}>
+                            <FormControl className={classes.formControl} >
+                                <InputLabel>Fornavn:</InputLabel>
+                                <Input />
+                            </FormControl>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <FormControl className={classes.formControl} >
+                                <InputLabel>Etternavn:</InputLabel>
+                                <Input />
+                            </FormControl>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <FormControl className={classes.formControl} >
+                                <InputLabel>Student-epost:</InputLabel>
+                                <Input />
+                            </FormControl>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <FormControl className={classes.formControl} >
+                                <InputLabel>Privat epost:</InputLabel>
+                                <Input />
+                            </FormControl>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <FormControl className={classes.formControl} >
+                                <InputLabel htmlFor="study-programme">Studieprogram</InputLabel>
+                                <Select
+                                    value={this.state.studyProgramme}
+                                    onChange={this.handleChange}
+                                    inputProps={{
+                                        name: 'studyProgramme',
+                                        id: 'study-programme',
+                                    }}
+                                    className={classes.selectEmpty}
+                                >
+                                    <MenuItem value={0}>MGLU1-7 - Grunnskolelærerutdanning 1.–7. trinn</MenuItem>
+                                </Select>
+                            </FormControl>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <FormControl className={classes.formControl} >
+                                <InputLabel htmlFor="year-of-admission">
+                                    Opptaksår
                         </InputLabel>
-                        <Select
-                            value={this.state.yearOfAdmission}
-                            onChange={this.handleChange}
-                            inputProps={{
-                                name: 'yearOfAdmission',
-                                id: 'year-of-admission',
-                            }}
-                        >
-                            <MenuItem value={2018}>2018</MenuItem>
-                            <MenuItem value={2017}>2017</MenuItem>
-                            <MenuItem value={2016}>2016</MenuItem>
-                        </Select>
-                    </FormControl>
-                    <FormControl>
-                        <InputLabel>Vipps transaksjonskode:</InputLabel>
-                        <Input />
-                    </FormControl>
-                    <FormControl>
-                        <InputLabel>Nyhetsbrev:</InputLabel>
-                        <Checkbox />
-                    </FormControl>
-                    <FormControl>
-                        <InputLabel>Terms of service:</InputLabel>
-                        <Checkbox />
-                    </FormControl>
-                    <FormControl>
-                        <Button variant="contained" color="primary">
-                            Registrer
-                    </Button>
-                    </FormControl>
+                                <Select
+                                    value={this.state.yearOfAdmission}
+                                    onChange={this.handleChange}
+                                    inputProps={{
+                                        name: 'yearOfAdmission',
+                                        id: 'year-of-admission',
+                                    }}
+                                    className={classes.selectEmpty}
+                                >
+                                    <MenuItem value={2018}>2018</MenuItem>
+                                    <MenuItem value={2017}>2017</MenuItem>
+                                    <MenuItem value={2016}>2016</MenuItem>
+                                </Select>
+                            </FormControl>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <FormControl className={classes.formControl} >
+                                <InputLabel>Vipps transaksjonskode:</InputLabel>
+                                <Input />
+                            </FormControl>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <div>
+                                <InputLabel>Nyhetsbrev:</InputLabel>
+                                <Checkbox />
+                            </div>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <div>
+                                <InputLabel>Terms of service:</InputLabel>
+                                <Checkbox />
+                            </div>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <FormControl className={classes.formControl} >
+                                <Button variant="contained" color="primary">
+                                    Registrer
+                                </Button>
+                            </FormControl>
+                        </Grid>
+                    </Grid>
                 </form>
             </div>
         );
@@ -93,4 +131,4 @@ class UserRegistrationForm extends Component {
 
 }
 
-export default UserRegistrationForm;
+export default withStyles(styles)(UserRegistrationForm);
