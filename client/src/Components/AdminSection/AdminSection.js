583249492ef69426db13e40405be58905916c242
Gard Steinsvik <gardsteinsvik@gmail.com>
2018-08-08 22:50:48 +0200

    Updated layout of action buttons in admin page

--- client/src/Components/AdminSection/AdminSection.js
+++ client/src/Components/AdminSection/AdminSection.js
@@ -1,4 +1,4 @@
-import { Button, Paper, Typography } from '@material-ui/core';
+import { Button, Paper, Typography, Grid } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
@@ -56,7 +56,7 @@ class AdminSection extends Component {
             this.setState({ redirectToLoginPage: true });
         }
 
-    }    
+    }
 
     onLogout = () => {
         this.props.onLogout();
@@ -74,16 +74,25 @@ class AdminSection extends Component {
         );
 
         const adminContent = (
-            <div>
+            <div className={classes.root}>
                 <Paper className={classes.paper}>
                     <Typography variant="headline" component="h3">Admin-panel</Typography>
                     <hr />
                     <br />
-                    <BulkActivate />
-                    <Button variant="contained" color="primary" onClick={this.onBulkActivate}>Bulk-aktivering</Button>
-                    <Button variant="contained" color="primary" onClick={console.log.bind('hei')}>Oppdater Terms of Service</Button>
-                    <Button variant="contained" color="primary" onClick={console.log.bind('hei')}>Eksporter nyhetsbrevliste</Button>
-                    <Button variant="contained" color="primary" onClick={console.log.bind('hei')}>Eksporter epostliste</Button>
+                    <Grid container spacing={8}>
+                        <Grid item xs={12} sm={6} md={4} lg={3}>
+                            <BulkActivate />
+                        </Grid>
+                        <Grid item xs={12} sm={6} md={4} lg={3}>
+                            <Button variant="contained" color="primary" onClick={console.log.bind('hei')}>Oppdater Terms of Service</Button>
+                        </Grid>
+                        <Grid item xs={12} sm={6} md={4} lg={3}>
+                            <Button variant="contained" color="primary" onClick={console.log.bind('hei')}>Eksporter nyhetsbrevliste</Button>
+                        </Grid>
+                        <Grid item xs={12} sm={6} md={4} lg={3}>
+                            <Button variant="contained" color="primary" onClick={console.log.bind('hei')}>Eksporter epostliste</Button>
+                        </Grid>
+                    </Grid>
                     <br />
                     <br />
                     <Button size="large" variant="contained" color="secondary" onClick={this.onLogout.bind(this)}>Logg ut</Button>
