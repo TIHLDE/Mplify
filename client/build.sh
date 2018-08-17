b0719006af483fb539271df4491480f68a4afc66
Harald Floor Wilhelmsen <harald.floor.wilhelmsen@trondheim.kommune.no>
2018-08-17 10:18:03 +0200

    Small build script fixes and formatting

--- client/build.sh
+++ client/build.sh
@@ -2,7 +2,11 @@ echo '-- npm run build --\n'
 
 npm run build
 
-echo '\n\n -- '
+echo '\n\n -- building docker image -- '
 
 docker build . -t tihlde/mplify-web:latest
+
+echo '\n\n -- pushing docker image -- '
+
+docker push tihlde/mplify-web:latest
 
