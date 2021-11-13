850a452e4d8c3fb87b95abdf1b1043249b2696b0
Bjørn Kristian Punsvik <bk.punsvik@gmail.com>
2021-11-13 17:32:42 +0100

    fix: re added api to compose and db
    added DB_HOST and DB_PORT to env_var

--- server/src/db.py
+++ server/src/db.py
@@ -3,11 +3,13 @@ from app import get_environ_sfe
 
 
 def init(loo):
-    global DB_PASSWORD, DB_USER, DB_NAME
+    global DB_PASSWORD, DB_USER, DB_NAME, DB_HOST, DB_PORT
     global loop
     DB_PASSWORD = get_environ_sfe('DB_PASSWORD')
     DB_USER = get_environ_sfe('DB_USER')
     DB_NAME = get_environ_sfe('DB_NAME')
+    DB_HOST = get_environ_sfe('DB_HOST')
+    DB_PORT = int(get_environ_sfe('DB_PORT'))
     loop = loo
 
 
@@ -15,7 +17,7 @@ async def mysql_connect():
     """ Returns a Connection-instance,
      and Cursor-instance from same connection
     """
-    conn = await aiomysql.connect(host='tihlde.org', port=3306,
+    conn = await aiomysql.connect(host=DB_HOST, port=DB_PORT,
                                   user=DB_USER, password=DB_PASSWORD,
                                   db=DB_NAME, loop=loop, cursorclass=aiomysql.DictCursor)
     cur = await conn.cursor()
